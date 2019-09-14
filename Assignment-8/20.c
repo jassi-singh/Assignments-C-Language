319480dc67e4d88e0ebcfd73656124c3831bd74f
Jassi <jaswinder.dandyan2000@gmail.com>
2019-09-14 12:42:17 +0530

    Update 20.c

--- Assignment-8/20.c
+++ Assignment-8/20.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int a,sum =0,ct=0;
+	int a,sum =0,n,k,ct=0;
 	printf("Enter the value n,k:");
 	scanf("%d%d",&n,&k);
 	printf("\nEnter the nos :");
@@ -10,9 +10,9 @@ int main()
 		scanf("%d",&a);
 		if(a%2==0)
 			ct++;
-		if(ct==1)
-			sum+=a; 
-	}		
-	primtf("SUM %d:",sum);
-	return o;
+		if(ct>k)
+			sum+=a;
+	}
+	printf("SUM %d:",sum);
+	return 0;
 }
